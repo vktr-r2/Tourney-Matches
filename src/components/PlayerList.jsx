481cda72f7d4a0f43ca7546535dc5b9e84474415
Viktor Ristic <vktr.r2@protonmail.com>
2023-05-15 17:04:22 -0400

    Import mock data into components

--- src/components/PlayerList.jsx
+++ src/components/PlayerList.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import Player from "./Player";
 
+import playerData from '../data/playerData';
+import matchData from '../data/matchData';
+
 function PlayerList() {
   return (
     <section className="PlayerList">
