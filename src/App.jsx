68e46aca0315fc2c910503d087ad17196dd1c3dc
Viktor Ristic <vktr.r2@protonmail.com>
2023-05-15 16:30:15 -0400

    Fix display bugs, import components into App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
-import './App.css';
+import "./App.css";
+import PlayerList from "./components/PlayerList";
+import MatchList from "./components/MatchList";
 
 function App() {
   return (
     <div className="App">
-      
+      <h1>
+        Tourney Matches{" "}
+        <span>Where Coding and Tournaments found their Match!</span>
+      </h1>
+      <PlayerList />
+      <MatchList />
     </div>
   );
 }
