a52cdc46e669c9975eab6175940e19c22e219393
Viktor Ristic <vktr.r2@protonmail.com>
2023-05-15 18:18:46 -0400

    Import mock data into PlayerList component

--- src/components/PlayerList.jsx
+++ src/components/PlayerList.jsx
@@ -4,7 +4,14 @@ import Player from "./Player";
 import playerData from '../data/playerData';
 import matchData from '../data/matchData';
 
-function PlayerList() {
+import {preparePlayerData, addWinsToPlayers} from '../helpers/playerHelpers'
+
+function PlayerList(props) {
+
+  const playerDataArray = preparePlayerData(playerData);
+  const parsedPlayerData = addWinsToPlayers(playerDataArray, matchData);
+  console.log(parsedPlayerData);
+
   return (
     <section className="PlayerList">
       <h1>Current participating players</h1>
